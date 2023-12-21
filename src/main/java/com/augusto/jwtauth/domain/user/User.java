00e40d4b1df356005524414bd6ef524b76dd2fad
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-20 23:18:10 -0300

    feat: creating the enum role for user

--- src/main/java/com/augusto/jwtauth/domain/user/User.java
+++ src/main/java/com/augusto/jwtauth/domain/user/User.java
@@ -6,9 +6,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Table(name = "users")
@@ -28,11 +30,12 @@ public class User implements UserDetails {
 
   private String password;
 
-  private String role;
+  private UserRole role;
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+    else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
   }
 
   @Override
