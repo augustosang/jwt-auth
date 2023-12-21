8618a528f33ed977329762235d57801cf9a44011
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-21 15:49:14 -0300

    feat: creating method to verify token

--- src/main/java/com/augusto/jwtauth/infra/security/TokenService.java
+++ src/main/java/com/augusto/jwtauth/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.augusto.jwtauth.domain.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -23,14 +24,28 @@ public class TokenService {
       return JWT.create()
               .withIssuer("auth-api")
               .withSubject(user.getLogin())
-              .withExpiresAt(genExprirationDate())
+              .withExpiresAt(genExpirationDate())
               .sign(algorithm);
     } catch(JWTCreationException exception) {
       throw  new RuntimeException("Error while generating token", exception);
     }
   }
 
-  private Instant genExprirationDate() {
+
+  public String validateToken(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      return JWT.require(algorithm)
+              .withIssuer("auth-api")
+              .build()
+              .verify(token)
+              .getSubject();
+    } catch (JWTVerificationException exception) {
+      return "";
+    }
+  }
+
+  private Instant genExpirationDate() {
     return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
   }
 }
