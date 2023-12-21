44a99034b2d1f8270991d851e9e72a9acf22924f
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-20 22:26:31 -0300

    feat: adding the product model

---
+++ src/main/java/com/augusto/jwtauth/domain/product/Product.java
@@ -0,0 +1,26 @@
+package com.augusto.jwtauth.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  private String name;
+
+  private Integer price;
+}
